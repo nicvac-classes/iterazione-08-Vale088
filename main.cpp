86e609d1b05af0561f491f3bfd05605f1b011c1d
Vale088 <valerio.sinni@marconibari.edu.it>
2024-10-16 14:26:15 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,32 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+int n,i; 
+string nome;
+float voto,somma,media;
+cout<<"quanti sono gli alunni?"<<endl;
+cin>>n;
+i=1;
+somma=0;
+while(i<=n)
+{ 
+i=i+1; 
+cout<<"inserisci il nome degli alunni"<<endl;
+cin>>nome;
+cout<<"inserisci il voto per ogni alunno"<<endl;
+cin>>voto;
+if(voto>=6)
+{ 
+ cout<<"l'alunno e stato promosso"<<endl; 
+    } 
+    else 
+    { 
+    cout<<"l'alunno non e stato promosso"<<endl;
+    }  
+    somma=somma+voto;
+    cout<<"la somma e "  somma<<endl;
 
+} 
+
+}
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
